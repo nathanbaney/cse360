27621b58f35f44388370fbc8f02fe098bc86cdb4
nathanbaney <Tmne4h4t>
2019-02-25 06:32:39 -0700

    Added method documentation

--- src/cse360assign2/Calculator.java
+++ src/cse360assign2/Calculator.java
@@ -1,34 +1,65 @@
-
+/**
+ * @author Nathan Baney
+ * ClassID: 322
+ * Calculator class for assignment 2
+ */
 package cse360assign2;
 
 public class Calculator {
 
 	private int total;
-	
+
+	/**
+	 * Constructor method
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+
+	/**
+	 * Returns the current accumulated total
+	 * @return The current calculated amount
+	 */
 	public int getTotal () {
 		return 0;
 	}
-	
+
+	/**
+	 * Adds the parameter to the current total
+	 * @param value The amount to be added
+	 */
 	public void add (int value) {
 		
 	}
-	
+
+	/**
+	 * Subtracts the parameter from the current total
+	 * @param value The amount to subtract
+	 */
 	public void subtract (int value) {
 		
 	}
-	
+
+	/**
+	 * Multiplies the current total by the parameter
+	 * @param value The multiplier to be applied
+	 */
 	public void multiply (int value) {
 		
 	}
-	
+
+	/**
+	 * Divides the current total by the parameter
+	 * @param value The dividend to be applied
+	 */
 	public void divide (int value) {
 		
 	}
-	
+
+	/**
+	 * Returns a string printout of the operations applied to the total
+	 * @return The string representation of the operation history
+	 */
 	public String getHistory () {
 		return "";
 	}
